b8004c70be980bc59f66cbcb4a4fa4e677831779
Albino Tonnnina <albino.tonnina@workshare.com>
2017-03-02 15:49:30 +0000

    get data from json file

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import NavComponent from './Nav';
-
 import styles from './styles/Header.scss';
 
-export default function () {
-    return (
-        <header className={styles.header}>
-            <a href="#main" className={styles.skipLink}>Skip to main content</a>
+export default (props) => (
+    <header className={styles.header}>
+        <a href="#main" className={styles.skipLink}>Skip to main content</a>
 
-            <div className={styles.head_top}>
+        <div className={styles.head_top}>
 
-                <img src="./assets/logo.png" alt="Deliveroo logo" className={styles.logo} />
-                <img src="./assets/logo-small.png" alt="Deliveroo logo" className={styles.logoSmall} />
+            <img src="./assets/logo.png" alt="Deliveroo logo" className={styles.logo}/>
+            <img src="./assets/logo-small.png" alt="Deliveroo logo" className={styles.logoSmall}/>
 
-                <div className={styles.navWrapper}>
-                    <NavComponent />
-                </div>
-
+            <div className={styles.navWrapper}>
+                <NavComponent />
             </div>
 
-            <section className={styles.head_bottom}>
-                <h1 className={styles.title}> <span className={styles.delivering}>[34] Restaurants delivering to</span><span className={styles.zone}>[ZONE]</span></h1>
-                <p className={styles.paragraph}>[Zone-information]</p>
-            </section>
+        </div>
+
+        <section className={styles.head_bottom}>
+            <h1 className={styles.title}>
+                <span className={styles.delivering}>{props.item_count} Restaurants delivering to</span>
+                <span className={styles.zone}>{props.area.name}</span>
+            </h1>
+            <p className={styles.paragraph}>{props.area.info}</p>
+        </section>
 
 
-        </header>
-    )
-}
+    </header>
+)
+
